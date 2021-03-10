cf3c4bdaa0a86a0346f3f38bff06deea19cd5222
Harpo <harpo@harpo.me>
2021-03-10 11:49:45 -0800

    added sysex header and allCall implementation

--- sandfly/boards.py
+++ sandfly/boards.py
@@ -1,9 +1,15 @@
 from sandfly import mixer
 from sandfly.specs import Midi, Mixer
+from rtmidi.midiconstants import NOTE_ON, CONTROL_CHANGE
 
 class Qu24(Midi, Mixer):
     def __init__(self, *args, **kwargs):
         super().__init__(self, *args, **kwargs)
+        self.header = [0xF0 , 0x0 , 0x0 , 0x1A , 0x50 , 0x11 , 0x1 , 0x0 , self.channel]
+
+    @mixer
+    def allCall(self):
+        self.midi.send_message(self.header[:-1] + [0x7F] + [0x10, 0x0, 0xF7])
     
     @mixer
     def fader(channel:int, volume: int):
