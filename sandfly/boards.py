35e82ef7c9b60bef1b30772edd6fc30fc8fa1843
Harpo <harpo@harpo.me>
2021-03-11 10:58:45 -0800

    implemented fader and pan for Qu

--- sandfly/boards.py
+++ sandfly/boards.py
@@ -12,5 +12,10 @@ class Qu24(Midi, Mixer):
         self.midi.send_message(self.header[:-1] + [0x7F] + [0x10, 0x0, 0xF7])
     
     @mixer
-    def fader(channel:int, volume: int):
+    def fader(self, channel:int, volume: int):
         print(self.client_name, 'setting channel volume', channel, volume)
+        self.nrpn(channel=channel, param=0x17, data1=volume, data2=0x7)
+
+    @mixer
+    def pan(self, channel:int, pan: int):
+        self.nrpn(channel, 0x16, pan, 0x7)
