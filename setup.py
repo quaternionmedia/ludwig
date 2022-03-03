c26aad92452c9ca9e3fcce13996e3ac957e0e51a
Harpo <harpo@harpo.me>
2022-03-03 11:27:21 -0800

    add pydantic to requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name='ludwig',
     install_requires=[
         'pluggy>=0.3,<1.0',
-        'python-rtmidi>=1.4.9,<1.5.0'
+        'python-rtmidi>=1.4.9,<1.5.0',
+        'pydantic>=1.9.0,<1.10.0',
     ],
     entry_points={'console_scripts': ['ludwig=ludwig.main:main']},
     packages=find_packages(),
