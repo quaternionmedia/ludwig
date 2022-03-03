8d61e9cccceef9a9a7597f1068bdfa618e8a5037
Harpo <harpo@harpo.me>
2022-03-03 10:50:01 -0800

    update install requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='ludwig',
-    install_requires='pluggy>=0.3,<1.0',
+    install_requires=[
+        'pluggy>=0.3,<1.0',
+        'python-rtmidi>=1.4.9,<1.5.0'
+    ],
     entry_points={'console_scripts': ['ludwig=ludwig.main:main']},
     packages=find_packages(),
 )
