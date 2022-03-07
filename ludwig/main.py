0f58b5d401dbc689b304841b8916eadc0e65722a
Harpo <harpo@harpo.me>
2022-03-07 10:30:41 -0800

    format code with black --skip-string-normalization

--- ludwig/main.py
+++ ludwig/main.py
@@ -2,25 +2,40 @@ from ludwig.specs import Mixer
 from ludwig.boards import Qu24
 from pluggy import PluginManager
 from argparse import ArgumentTypeError
+
+
 def channel(n: int):
     n = int(n)
-    if n>109:
+    if n > 109:
         raise ArgumentTypeError('invalid QU channel number')
     else:
         return n
+
 
 def main():
     pm = get_plugin_manager()
     try:
         from argparse import ArgumentParser
+
         parser = ArgumentParser(description='remote sound board operation')
-        parser.add_argument('channels', metavar='N', type=channel, nargs='+',
-                            help='an integer for the accumulator')
+        parser.add_argument(
+            'channels',
+            metavar='N',
+            type=channel,
+            nargs='+',
+            help='an integer for the accumulator',
+        )
         group = parser.add_mutually_exclusive_group()
-        group.add_argument('-m', '--mute', dest='mute', action='store_true',
-                    help='mute the channel(s)')
-        group.add_argument('-u', '--unmute', dest='unmute', action='store_true',
-                    help='unmute the channel(s)')
+        group.add_argument(
+            '-m', '--mute', dest='mute', action='store_true', help='mute the channel(s)'
+        )
+        group.add_argument(
+            '-u',
+            '--unmute',
+            dest='unmute',
+            action='store_true',
+            help='unmute the channel(s)',
+        )
         args = parser.parse_args()
 
         args = parser.parse_args()
@@ -36,6 +51,7 @@ def main():
         print(e)
     finally:
         pm.hook.close()
+
 
 def get_plugin_manager():
     pm = PluginManager('mixer')
