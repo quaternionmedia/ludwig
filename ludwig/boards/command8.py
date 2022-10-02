b8860269aec6822694407e1a810f8d44dca2951d
Harpo <harpo@harpo.me>
2022-10-01 17:34:17 -0700

    Enable fader hook: Working fader control!!!

--- ludwig/boards/command8.py
+++ ludwig/boards/command8.py
@@ -26,3 +26,13 @@ class Command8(Midi, Mixer):
         """Unmute the channel"""
         self.send([CONTROL_CHANGE | channel, 14, 0])
 
+    def __call__(self, event, data=None):
+        message, deltatime = event
+        print('Command8!', message, deltatime)
+        if message[0] & 0xF0 == CONTROL_CHANGE:
+            print(
+                f'found control change message {message[0]} on channel {message[0] & 0x0F}'
+            )
+            if message[1] == 7:
+                print(f'fader channel {message[0] & 0x0F } at {message[2]}')
+                self.hook.fader(channel=message[0] & 0x0F, volume=message[2])
