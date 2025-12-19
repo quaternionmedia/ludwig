a39276a3135f4f1b14ad1f30fcce459fb5d33b30
Harpo <harpo@harpo.me>
2025-12-19 00:36:09 -0800

    Migrates XAir to new architecture

--- src/ludwig/boards/__init__.py
+++ src/ludwig/boards/__init__.py
@@ -1,5 +1,7 @@
 from .qu24 import Qu24
 from .gld import Gld
-from .xair import XAir
+from .xair import XAir, XAirPlugin
 from .command8 import Command8
 from .generic import Generic
+
+__all__ = ["Qu24", "Gld", "XAir", "XAirPlugin", "Command8", "Generic"]
